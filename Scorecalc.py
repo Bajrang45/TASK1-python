94e3ae49f9d289601696b9c584c094c031aee48d
Bajrang45 <tokalwadbajrang123@gmail.com>
2026-06-28 22:04:40 +0530

    Add files via upload

---
+++ Scorecalc.py
@@ -0,0 +1,7 @@
+score1 = int(input("Enter Score of Level 1: "))
+score2 = int(input("Enter Score of Level 2: "))
+score3 = int(input("Enter Score of Level 3: "))
+
+total = score1 + score2 + score3
+
+print("Total Score =", total)
