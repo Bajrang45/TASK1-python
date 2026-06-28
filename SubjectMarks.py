421344438efdcd6c1cb60510a99db0e188c020c3
Bajrang45 <tokalwadbajrang123@gmail.com>
2026-06-28 20:24:05 +0530

    Add files via upload

---
+++ SubjectMarks.py
@@ -0,0 +1,9 @@
+sub1 = 80
+sub2 = 75
+sub3 = 90
+sub4 = 88
+sub5 = 95
+
+total = sub1 + sub2 + sub3 + sub4 + sub5
+
+print("Total Marks =", total)
---
+++ Variables.py
@@ -0,0 +1,9 @@
+name = "Bajrang"
+age = 20
+city = "Nanded"
+college = "Gramin  Polytechnic College"
+
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
+print("College:", college)
