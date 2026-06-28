b8fdc2a016a61411374673260fe105d41c02f3a7
Bajrang45 <tokalwadbajrang123@gmail.com>
2026-06-28 21:44:48 +0530

    Add files via upload
    Arithmetic operation

---
+++ AreaOfRect.py
@@ -0,0 +1,14 @@
+# Rectangle
+length = float(input("Enter length: "))
+breadth = float(input("Enter breadth: "))
+
+rectangle_area = length * breadth
+
+print("Area of Rectangle =", rectangle_area)
+
+# Circle
+radius = float(input("Enter radius: "))
+
+circle_area = 3.14 * radius * radius
+
+print("Area of Circle =", circle_area)
